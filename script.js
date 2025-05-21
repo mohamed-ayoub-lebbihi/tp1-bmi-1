5c8119f2fa97aaaa3a9b3d638e213895e893b05a
mohamed-ayoub-lebbihi <ayoublibbihi@gmail.com>
2025-05-21 19:07:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+function validateForm() {
+    var name = document.getElementById('name').value.trim();
+    var weight = document.getElementById('weight').value.trim();
+    var height = document.getElementById('height').value.trim();
+  
+    if (name === "" || weight === "" || height === "") {
+      alert("All fields are required.");
+      return false;
+    }
+  
+    if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
+      alert("Weight and Height must be valid positive numbers.");
+      return false;
+    }
+  
+    return true;
+  }
+  
